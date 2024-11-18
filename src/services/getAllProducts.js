7153f78e77080157abcf6b8c2da0e191a781e139
nurgian <muhammadnurgian77@gmail.com>
2024-11-18 15:18:31 +0700

    first commit

--- src/services/getAllProducts.js
+++ src/services/getAllProducts.js
@@ -33,5 +33,60 @@ export default function getAllProducts() {
         imageUrl: `/assets/images/Mercedes Benz C Class.jpg`,
         description: `Mercedes-Benz C-Class adalah perpaduan sempurna antara desain elegan, performa superior, dan teknologi mutakhir. Dengan garis eksterior yang dinamis, grille ikonik, dan lampu LED adaptif, C-Class menawarkan akselerasi mulus dan handling presisi. Dilengkapi fitur keselamatan seperti Active Brake Assist, kamera 360°, dan teknologi semi-otonom, Mercedes-Benz C-Class memberikan pengalaman berkendara yang aman, nyaman, dan penuh gaya. Mercedes-Benz C-Class–The Future of Luxury Driving.`
       },
+      {
+        id: 'ATTC-06-SCAR-004',
+        slug: 'audi-tt-coupe-06-sport',
+        name: "Audi TT Coupe '06",
+        category: `Sport's Car`,
+        categorySlug: 'sport-car',
+        price: 1_425_000_000,
+        stock: 42,
+        imageUrl: `/assets/images/Audi TT Coupe.jpg`,
+        description: `Mercedes-Benz C-Class adalah perpaduan sempurna antara desain elegan, performa superior, dan teknologi mutakhir. Dengan garis eksterior yang dinamis, grille ikonik, dan lampu LED adaptif, C-Class menawarkan akselerasi mulus dan handling presisi. Dilengkapi fitur keselamatan seperti Active Brake Assist, kamera 360°, dan teknologi semi-otonom, Mercedes-Benz C-Class memberikan pengalaman berkendara yang aman, nyaman, dan penuh gaya. Mercedes-Benz C-Class–The Future of Luxury Driving.`
+      },
+      {
+        id: 'BB-W12-16-SCAR-005',
+        slug: 'bentley-bentayga-w12-16-sport',
+        name: "Bentley Bentayga W12 '16",
+        category: `Sport's Car`,
+        categorySlug: 'sport-car',
+        price: 8_099_000_000,
+        stock: 6,
+        imageUrl: `/assets/images/Bentley Bentayga W12.jpg`,
+        description: `Mercedes-Benz C-Class adalah perpaduan sempurna antara desain elegan, performa superior, dan teknologi mutakhir. Dengan garis eksterior yang dinamis, grille ikonik, dan lampu LED adaptif, C-Class menawarkan akselerasi mulus dan handling presisi. Dilengkapi fitur keselamatan seperti Active Brake Assist, kamera 360°, dan teknologi semi-otonom, Mercedes-Benz C-Class memberikan pengalaman berkendara yang aman, nyaman, dan penuh gaya. Mercedes-Benz C-Class–The Future of Luxury Driving.`
+      },
+      {
+        id: 'MMRF-24-SCAR-006',
+        slug: 'mazda-mx5-rf-24-sport',
+        name: "Mazda MX5 RF '24",
+        category: `Sport's Car`,
+        categorySlug: 'sport-car',
+        price: 945_500_000,
+        stock: 0,
+        imageUrl: `/assets/images/MAZDA MX5 RF .jpg`,
+        description: `Mercedes-Benz C-Class adalah perpaduan sempurna antara desain elegan, performa superior, dan teknologi mutakhir. Dengan garis eksterior yang dinamis, grille ikonik, dan lampu LED adaptif, C-Class menawarkan akselerasi mulus dan handling presisi. Dilengkapi fitur keselamatan seperti Active Brake Assist, kamera 360°, dan teknologi semi-otonom, Mercedes-Benz C-Class memberikan pengalaman berkendara yang aman, nyaman, dan penuh gaya. Mercedes-Benz C-Class–The Future of Luxury Driving.`
+      },
+      {
+        id: 'P911-18-SCAR-007',
+        slug: 'porsche-911-18-sport',
+        name: "Porsche 911 '18",
+        category: `Sport's Car`,
+        categorySlug: 'sport-car',
+        price: 4_900_000_000,
+        stock: 50,
+        imageUrl: `/assets/images/Porsche 911.jpg`,
+        description: `Mercedes-Benz C-Class adalah perpaduan sempurna antara desain elegan, performa superior, dan teknologi mutakhir. Dengan garis eksterior yang dinamis, grille ikonik, dan lampu LED adaptif, C-Class menawarkan akselerasi mulus dan handling presisi. Dilengkapi fitur keselamatan seperti Active Brake Assist, kamera 360°, dan teknologi semi-otonom, Mercedes-Benz C-Class memberikan pengalaman berkendara yang aman, nyaman, dan penuh gaya. Mercedes-Benz C-Class–The Future of Luxury Driving.`
+      },
+      {
+        id: 'TS-20-SCAR-008',
+        slug: 'toyota-supra-20-sport',
+        name: "Toyota Supra '20",
+        category: `Sport's Car`,
+        categorySlug: 'sport-car',
+        price: 2_238_000_000,
+        stock: 179,
+        imageUrl: `/assets/images/Toyota Supra.jpg`,
+        description: `Mercedes-Benz C-Class adalah perpaduan sempurna antara desain elegan, performa superior, dan teknologi mutakhir. Dengan garis eksterior yang dinamis, grille ikonik, dan lampu LED adaptif, C-Class menawarkan akselerasi mulus dan handling presisi. Dilengkapi fitur keselamatan seperti Active Brake Assist, kamera 360°, dan teknologi semi-otonom, Mercedes-Benz C-Class memberikan pengalaman berkendara yang aman, nyaman, dan penuh gaya. Mercedes-Benz C-Class–The Future of Luxury Driving.`
+      },
     ]
   }
