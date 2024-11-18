d7e2c4fa64671ffecc4675e0e533ee46d2cca07f
nurgian <muhammadnurgian77@gmail.com>
2024-11-18 10:07:43 +0700

    first commit

---
+++ src/services/getAllProductCategories.jsx
@@ -0,0 +1,15 @@
+const getAllProductCategories = ()  => {
+    return [   {
+      id: 'SCAR',
+      slug: 'sport-car',
+      name: `Sport's Car`
+    },
+    {
+      id: 'SECAR',
+      slug: 'sedan-car',
+      name: `Sedan's Car`
+    },
+    ]
+  }
+  
+  export default getAllProductCategories
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,37 @@
+export default function getAllProducts() {
+    return [
+      {
+        id: 'TG86-12-SCAR-001',
+        slug: 'Toyota-GR-86-12-sport',
+        name: "Toyota GR 86 '12",
+        category: `Sport's Car`,
+        categorySlug: 'sport-car',
+        price: 1_054_000_000,
+        stock: 100,
+        imageUrl: '/assets/images/Toyota GR 86.jpg',
+        description: `Toyota GR 86 adalah mobil sport penuh adrenalin dengan mesin Boxer 2.4L bertenaga 228 hp yang menawarkan akselerasi responsif dan stabilitas tinggi. Desain aerodinamisnya memukau dengan grille khas GR, lampu LED tajam, dan interior modern lengkap dengan layar sentuh 8 inci serta konektivitas canggih. Dengan pilihan transmisi manual atau otomatis dan suspensi presisi, GR 86 memberikan pengalaman berkendara yang mendebarkan di setiap lintasan. Toyota GR 86-kecepatan dalam gaya, siap menemani perjalanan Anda!`
+      },
+      {
+        id: 'AA7-10-SCAR-002',
+        slug: 'audi-a7-10-sport',
+        name: "Audi A7 '10",
+        category: `Sport's Car`,
+        categorySlug: 'sport-car',
+        price: 1_895_000_000,
+        stock: 5,
+        imageUrl: `/assets/images/Audi A7.jpg`,
+        description: `Audi A7 adalah spesies baru untuk Audi. Mobil ini digadang untuk menghantam Mercedes-Benz CLS yang sukses dengan desain kombinasi antara sedan dan coupe. Desain khas Audi ternyata mampu memberikan sentuhan baru yang efeknya luar biasa. Bentuknya terlihat keren, sekaligus berwibawa.`
+      },
+      {
+        id: 'MBCC-14-SECAR-003',
+        slug: 'marcedes-benz-c-class-14-sedan',
+        name: "Marcedes Benz C Class '14",
+        category: `Sedan's Car`,
+        categorySlug: 'sedan-car',
+        price: 1_385_000_000,
+        stock: 0,
+        imageUrl: `/assets/images/Mercedes Benz C Class.jpg`,
+        description: `Mercedes-Benz C-Class adalah perpaduan sempurna antara desain elegan, performa superior, dan teknologi mutakhir. Dengan garis eksterior yang dinamis, grille ikonik, dan lampu LED adaptif, C-Class menawarkan akselerasi mulus dan handling presisi. Dilengkapi fitur keselamatan seperti Active Brake Assist, kamera 360°, dan teknologi semi-otonom, Mercedes-Benz C-Class memberikan pengalaman berkendara yang aman, nyaman, dan penuh gaya. Mercedes-Benz C-Class–The Future of Luxury Driving.`
+      },
+    ]
+  }
